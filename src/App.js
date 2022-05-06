ee48700ecf8815d8f21a126c5cae65ec9a185110
Alexander Sherwin <alx.sherwin@gmail.com>
2022-05-06 15:52:25 -0600

    WOOHOO! Figured out why the fetch wasn't working -- went back to rick and morty api docs and fixed issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { useEffect } from 'react';
 import './App.css';
 
+const API_URL = "https://rickandmortyapi.com/api";
+
 function App() {
+
+  const searchRickNMorty = async (name) => {
+    const response = await fetch(`${API_URL}/character/?=${name}`);
+
+    const data = await response.json();
+    console.log(data);
+  }
+  
+  useEffect(() => {
+    searchRickNMorty("Rick");
+  }, []);
+
   return (
     <div className="App">
      Hello 
