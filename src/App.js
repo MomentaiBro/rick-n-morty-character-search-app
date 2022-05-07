9692e6d171feadd3d49d2efdb761054fdb9bdc8f
Alexander Sherwin <alx.sherwin@gmail.com>
2022-05-07 13:45:05 -0600

    redid the readme

--- src/App.js
+++ src/App.js
@@ -6,28 +6,6 @@ import CharacterCard from './CharacterCard';
 
 const API_URL = "https://rickandmortyapi.com/api";
 
-const characterData1 = {
-  "id": 14,
-  "name": "Alien Morty",
-  "status": "unknown",
-  "species": "Alien",
-  "type": "",
-  "gender": "Male",
-  "origin": {
-      "name": "unknown",
-      "url": ""
-  },
-  "location": {
-      "name": "Citadel of Ricks",
-      "url": "https://rickandmortyapi.com/api/location/3"
-  },
-  "image": "https://rickandmortyapi.com/api/character/avatar/14.jpeg",
-  "episode": [
-      "https://rickandmortyapi.com/api/episode/10"
-  ],
-  "url": "https://rickandmortyapi.com/api/character/14",
-  "created": "2017-11-04T20:51:31.373Z"
-}
 
 function App() {
 
